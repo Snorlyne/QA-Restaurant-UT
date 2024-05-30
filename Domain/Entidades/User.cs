b5add371dbd02add252d36a88e7eba20936ff755
Lian Erick Aguirre Sierra <113861220+Snorlyne@users.noreply.github.com>
2024-05-30 18:00:47 -0500

    feat: Creación de dbSeeder y entidades rol y user con migration

---
+++ Domain/Entidades/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entidades
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [ForeignKey("Role")]
+        public int FK_Rol_Id { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
---
+++ Repository/Context/ApplicationDbSeeder.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Repository.Repositorio.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Context
+{
+    public class ApplicationDbSeeder
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private bool _seeded;
+
+        public ApplicationDbSeeder(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task EnsureSeed()
+        {
+            if (!_seeded)
+            {
+                try
+                {
+                    User existingUser = await _dbContext.User.FirstOrDefaultAsync(x => x.Email == "root@root.com");
+                    if (existingUser != null)
+                    {
+                        Console.WriteLine("Database already seeded!");
+                        _seeded = true;
+                        return;
+                    }
+                    string[] roleNames = { "Root", "Admin", "Chef", "Waiter","Cashier" };
+                    foreach (var roleName in roleNames)
+                    {
+                        Role role = new();
+
+                        var roleExist = await _dbContext.Role.FirstOrDefaultAsync(x => x.Nombre == roleName);
+                        if (roleExist == null)
+                        {
+                            role.Nombre = roleName;
+                            _dbContext.Role.Add(role);
+                            await _dbContext.SaveChangesAsync();
+                        }
+                    }
+
+                    User user = new()
+                    {
+                        Email = "root@root.com",
+                        Password = "QA-RestaurantRALL123",
+                        FK_Rol_Id = 1
+                    };
+                    _dbContext.Add(user);
+                    await _dbContext.SaveChangesAsync();
+
+                    _seeded = true;
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine("Error trying to seed the database");
+                    Console.Error.WriteLine(ex);
+                    return;
+                }
+             }
+        }
+
+    }
+}
