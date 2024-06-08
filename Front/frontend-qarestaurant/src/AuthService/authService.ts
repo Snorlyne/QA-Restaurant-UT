a1dceba56a1e2e78bed3b415ee9fe02cc61e3c66
LuisFuen1222 <118927684+LuisFuen1222@users.noreply.github.com>
2024-06-07 23:41:28 -0500

    Login
    Login hecho con conexiones a la BD

--- Front/frontend-qarestaurant/src/AuthService/authService.ts
+++ Front/frontend-qarestaurant/src/AuthService/authService.ts
@@ -1,17 +1,24 @@
+import axios from 'axios';
 
 const authService = {
     login: (email: string, password: string): Promise<void> => {
       return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (email === '' && password === '') {
-            resolve();
-          } else {
-            reject('error');
-          }
-        }, 1000);
+        axios.post('https://localhost:7047/APIAuth/Login', {
+          email: email,
+          password: password
+        })
+        .then(function (response) {
+          localStorage.setItem('token', response.data.result.jwTtoken);
+          resolve(response.data.result.role);
+          console.log();
+
+        })
+        .catch(function (error) {
+          console.error(error);
+          reject('error');
+        });
       });
     }
-  };
-  
-  export default authService;
-  
+};
+
+export default authService;
