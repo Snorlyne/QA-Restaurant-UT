68f29be870ca8b1c195af108fbed35f05942d239
LuisFuen1222 <118927684+LuisFuen1222@users.noreply.github.com>
2024-06-11 00:58:23 -0500

    Reapply "Bien"
    This reverts commit 07b7b19efd7fb9c589ba24e0e7e413404bfe5179.

---
+++ BaseWeb/Controllers/API/APIInventarioController.cs
@@ -0,0 +1,49 @@
+﻿using Domain.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.IServicio;
+using System.Threading.Tasks;
+
+namespace BaseWeb.Controllers.API
+{
+    [ApiController]
+    [Authorize(Roles = "Root")]
+    [Route("[controller]")]
+    public class APIInventarioController : ControllerBase
+    {
+        private readonly IInventarioServicio _inventarioService;
+
+        public APIInventarioController(IInventarioServicio inventarioService)
+        {
+            _inventarioService = inventarioService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerLista()
+        {
+            var result = await _inventarioService.ObtenerInventario();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromBody] InventarioVM request)
+        {
+            var result = await _inventarioService.CrearProducto(request);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Actualizar(int id, [FromBody] InventarioVM inventario)
+        {
+            var result = await _inventarioService.ActualizarProducto(id, inventario);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var result = await _inventarioService.EliminarProducto(id);
+            return Ok(result);
+        }
+    }
+}
